86396f5ee394e1750cc4b878c86fc430c80bb09d
Alex Colombo <uricaine@hotmail.com>
2017-05-31 17:54:27 +0200

    new

--- src/app/components/producto-add.components.ts
+++ src/app/components/producto-add.components.ts
@@ -11,69 +11,76 @@ import { GLOBAL } from './../services/global';
 @Component({
     selector: 'producto-add',
     templateUrl: '../views/producto-add.html',
-    providers:[ProductoService]
+    providers: [ProductoService]
 })
 
-export class ProductoAddComponent{
-       public titulo:string;
-       public producto: Producto;
-        public filesToUpload;
-       public resultUpload;
+export class ProductoAddComponent {
+    public titulo: string;
+    public producto: Producto;
+    public filesToUpload;
+    public resultUpload;
 
-       constructor(
-           private _route: ActivatedRoute,
-           private _router: Router,
-           private _productoService: ProductoService
-       ){
-           this.titulo = "Crear un nuevo producto"
-           this.producto = new Producto(0, "", "", 0, "");
-       }
-       ngOnInit(){
-           console.log('¡¡¡¡Producto-add.component cargado!!!');     
-       }
+    constructor(
+        private _route: ActivatedRoute,
+        private _router: Router,
+        private _productoService: ProductoService
+    ) {
+        this.titulo = "Crear un nuevo producto"
+        this.producto = new Producto(0, "", "", 0, "");
+    }
+    ngOnInit() {
+        console.log('¡¡¡¡Producto-add.component cargado!!!');
+    }
 
-       onSubmit(){
+    onSubmit() {
         console.log(this.producto);
 
-         this._productoService.makeFileRequest(GLOBAL.url+'upload-file', [], this.filesToUpload).then((result)=>{
-                              
-                                  console.log(result);
-                                  
 
 
-                             }, (error)=>{
-                                 console.log(error);
-                                 
-                             }
-                             
-                             )
- 
+       if(this.filesToUpload.length >= 1){
+
+        this._productoService.makeFileRequest(GLOBAL.url + 'upload-file', [], this.filesToUpload).then((result) => {
+            console.log(result);
+            this.resultUpload = result;
+            this.producto.imagen = this.resultUpload.filename;
+            console.log(this.producto.imagen);
+            this.saveProducto();
+
+            }, (error) => {
+            console.log(error);
+            });
+            }else{
+            this.saveProducto(); 
+            }
 
 
 
-
-           
-             this._productoService.addProductos(this.producto).subscribe(
-               response => {
-                     console.log(response.code);
-                         if(response.code == 200){ 
-                         }else{
-                            console.log(response); 
-                        }
-                         },               
-                 error => {
-                       console.log(<any>error);
-                       
-                 }
-                 );
-               this._router.navigate(['/productos']);
+        
+    }
 
 
-       } 
+    saveProducto() {
+        this._productoService.addProductos(this.producto).subscribe(
+            response => {
+                console.log(response);
+                if (response.code == 200) {
+                    this._router.navigate(['/productos']);
+                } else {
+                    console.log(response);
+                }
+            },
+            error => {
+                console.log(<any>error);
+            });
 
-       fileChangeEvent(fileInput: any){
-		this.filesToUpload = <Array<File>>fileInput.target.files;
-		console.log(this.filesToUpload);
-	}
-      
+
+    }
+
+
+
+    fileChangeEvent(fileInput: any) {
+        this.filesToUpload = <Array<File>>fileInput.target.files;
+        console.log(this.filesToUpload);
+    }
+
 }
